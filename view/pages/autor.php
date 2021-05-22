0dfc77b7120b2a3e059f2f1719ef333ebd090d12
Jovanovic Milica <46057613+jovanovicmilica@users.noreply.github.com>
2021-05-22 15:38:30 +0200

    Update autor.php

--- view/pages/autor.php
+++ view/pages/autor.php
@@ -7,13 +7,12 @@
             <img src="assets/images/jana.jpg" alt="Autor">
         </div>
         <div class="tekst">
-        <h2>Jana Matović 139/19</h2>
+        <h2>Milica Jovanovicć 88/18</h2>
 
         <p>Smer- Internet tehnologije</p>
         <p>Modul- Web programiranje</p>
             
-        <p>Zovem se Jana Matović i rođena sam u Prijepolju. Završila sam Prijepoljsku gimnaziju, prirodno - matematički smer. Oduvek sam bila odusevljena računarima, pa sam zbog toga i upisala programiranje. Pored maternjeg jezika, znam engleski i malo nemački.
-        </p>
+        
 
         </div>
     </div>
